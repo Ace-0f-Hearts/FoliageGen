95771b85dd705ce16b4812000e569735084d3268
Ace-of-Heartz <synthetace@gmail.com>
2026-01-29 19:07:39 +0100

    Added types and formats for OCAD file imports

---
+++ src/mapfiles/OcdTypesV10.h
@@ -0,0 +1,17 @@
+//
+// Created by ace on 2026-01-26.
+//
+
+#ifndef GENERATOR_OCDTYPESV10_H
+#define GENERATOR_OCDTYPESV10_H
+#include "OcdTypesV9.h"
+
+
+class OcdTypesV10
+{
+    public:
+    using Format = OcdTypesV9::Format;
+};
+
+
+#endif //GENERATOR_OCDTYPESV10_H

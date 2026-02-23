229468418e933f5c4caae4c74cdee2d87594d33d
Ace-of-Heartz <synthetace@gmail.com>
2026-02-23 18:43:42 +0100

    Added file format registry

---
+++ include/ocad/file_format_registry.h
@@ -0,0 +1,34 @@
+//
+// Created by ace on 2026-02-18.
+//
+
+#ifndef PROCEDURALFOLIAGEGENERATOR_FILE_FORMAT_REGISTRY_H
+#define PROCEDURALFOLIAGEGENERATOR_FILE_FORMAT_REGISTRY_H
+#include <filesystem>
+#include <functional>
+#include <string>
+#include <vector>
+
+#include "file_format.h"
+#include "orienteering/map.h"
+
+class FileFormatRegistry
+{
+    using string = std::string;
+
+public:
+    FileFormatRegistry();
+    FileFormatRegistry(const FileFormat& file_format) = delete;
+    FileFormatRegistry& operator=(FileFormatRegistry) = delete;
+    FileFormatRegistry& operator=(FileFormatRegistry const&) = delete;
+
+    [[nodiscard]] std::unique_ptr<Importer>  CreateImporter(const std::filesystem::path& path, Orienteering::Map& map) const;
+
+    std::vector<FileFormat*>& file_formats();
+private:
+    const FileFormat* FindFormat(std::function<bool (const FileFormat*)> predicate) const;
+    const FileFormat* FindFormatForData(const std::filesystem::path& path) const;
+    std::vector<FileFormat*> file_formats_;
+};
+
+#endif //PROCEDURALFOLIAGEGENERATOR_FILE_FORMAT_REGISTRY_H

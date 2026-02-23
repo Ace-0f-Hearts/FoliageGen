c6fdd0226b471d10e2f0c9e1a871411924c6672a
Ace-of-Heartz <synthetace@gmail.com>
2026-02-23 18:44:03 +0100

    Implemented OCAD file format for importing

---
+++ include/ocad/ocad_file_format.h
@@ -0,0 +1,19 @@
+//
+// Created by ace on 2026-02-18.
+//
+
+#ifndef PROCEDURALFOLIAGEGENERATOR_OCAD_FILE_FORMAT_H
+#define PROCEDURALFOLIAGEGENERATOR_OCAD_FILE_FORMAT_H
+#include "file_format.h"
+
+class OcadFileFormat : FileFormat
+{
+public:
+    OcadFileFormat();
+    ~OcadFileFormat() override;
+    std::unique_ptr<Importer>  CreateImporter(std::filesystem::path path, Orienteering::Map map) const override;
+    bool UnderstandsHeader(const char *buffer, int total_read) const override;
+
+};
+
+#endif //PROCEDURALFOLIAGEGENERATOR_OCAD_FILE_FORMAT_H
---
+++ src/ocad/ocad_file_format.cpp
@@ -0,0 +1,27 @@
+//
+// Created by ace on 2026-02-18.
+//
+
+#include <ocad/ocad_file_format.h>
+
+#include "ocad/ocad_importer.h"
+
+OcadFileFormat::OcadFileFormat()
+{
+}
+
+OcadFileFormat::~OcadFileFormat()
+{
+}
+
+std::unique_ptr<Importer> OcadFileFormat::CreateImporter(std::filesystem::path path, Orienteering::Map map) const
+{
+    return std::make_unique<Ocad::OcadImporter>(path, map);
+}
+
+bool OcadFileFormat::UnderstandsHeader(const char* buffer, int total_read) const
+{
+    if (total_read >= 2 && static_cast<uint8_t>(buffer[0]) == 0xAD && buffer[1] == 0x0C)
+        return true;
+    return false;
+}

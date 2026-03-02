253dd50ab6bba2a77a6479afdf61cce20fc47e3d
Ace-of-Heartz <synthetace@gmail.com>
2026-03-02 17:52:58 +0100

    Added support for ocad version 2018

---
+++ include/ocad/ocad_types_v2018.h
@@ -0,0 +1,18 @@
+//
+// Created by ace on 2026-03-02.
+//
+
+#ifndef PROCEDURALFOLIAGEGENERATOR_OCAD_TYPES_V2018_H
+#define PROCEDURALFOLIAGEGENERATOR_OCAD_TYPES_V2018_H
+#include "ocad_types_v12.h"
+
+
+namespace Ocad
+{
+    class OcadTypesV2018
+    {
+    public:
+        using Format = OcadTypesV12::Format;
+    };
+}
+#endif //PROCEDURALFOLIAGEGENERATOR_OCAD_TYPES_V2018_H

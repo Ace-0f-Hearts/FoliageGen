bd325930d7c9e571d1db11a236afe8e71463d729
Ace-of-Heartz <synthetace@gmail.com>
2026-03-02 15:58:01 +0100

    Added structure dimension related queries

---
+++ include/botanics_math/dim.h
@@ -0,0 +1,14 @@
+//
+// Created by ace on 2026-02-26.
+//
+
+#ifndef PROCEDURALFOLIAGEGENERATOR_DIM_H
+#define PROCEDURALFOLIAGEGENERATOR_DIM_H
+
+struct Dim2
+{
+    int x;
+    int y;
+};
+
+#endif //PROCEDURALFOLIAGEGENERATOR_DIM_H

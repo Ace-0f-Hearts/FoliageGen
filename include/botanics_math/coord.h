37d2336568689042c3ffb66246f583261dbe88fa
Ace-of-Heartz <synthetace@gmail.com>
2026-03-02 15:57:56 +0100

    Added structure coordinate related queries

---
+++ include/botanics_math/coord.h
@@ -0,0 +1,24 @@
+//
+// Created by ace on 2026-02-26.
+//
+
+#ifndef PROCEDURALFOLIAGEGENERATOR_COORD_H
+#define PROCEDURALFOLIAGEGENERATOR_COORD_H
+
+struct Coord2
+{
+    int x;
+    int y;
+};
+
+inline Coord2 operator+(Coord2 const& a, Coord2 const& b)
+{
+    return Coord2{a.x + b.x, a.y + b.y};
+}
+
+inline Coord2 operator-(Coord2 const& a, Coord2 const& b)
+{
+    return Coord2{a.x - b.x, a.y - b.y};
+}
+
+#endif //PROCEDURALFOLIAGEGENERATOR_COORD_H

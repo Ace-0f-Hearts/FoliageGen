ef72062abacf92f305f8258802be7e708361435b
Ace-of-Heartz <synthetace@gmail.com>
2026-03-12 15:03:54 +0100

    Added leftshift operator overload for integer coordinates

--- include/botanics_math/coord.h
+++ include/botanics_math/coord.h
@@ -5,6 +5,7 @@
 #ifndef PROCEDURALFOLIAGEGENERATOR_COORD_H
 #define PROCEDURALFOLIAGEGENERATOR_COORD_H
 
+#include <fstream>
 struct Coord2
 {
     int x;
@@ -19,6 +20,12 @@ inline Coord2 operator+(Coord2 const& a, Coord2 const& b)
 inline Coord2 operator-(Coord2 const& a, Coord2 const& b)
 {
     return Coord2{a.x - b.x, a.y - b.y};
+}
+
+inline std::ostream& operator<< (std::ostream& os, Coord2 const& v)
+{
+    os << "(" << v.x << ", " << v.y << ")";
+    return os;
 }
 
 #endif //PROCEDURALFOLIAGEGENERATOR_COORD_H
